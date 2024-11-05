802048e5897642b5c3e84cb1f395950e8e4a4fb9
Finetundra <19881423+Finetundra@users.noreply.github.com>
2024-11-04 20:57:48 -0500

    Add files via upload

---
+++ Software/iexplore.bat
@@ -0,0 +1,4 @@
+rem This is a horrible little script that invokes an Internet Explorer 11 window. Borrowed it from somewhere else on the internet, sorry to whoever I grabbed it from, I don't remember who you are, but I'm sure documentation is out there to explain everything going on here.
+rem I'm uploading this in case it happens to be useful, but frankly, I can't see why you would use IE11 for *anything* aside from very specific internal tooling, in which case, you're probably not using a batch script uploaded by a random person. Or maybe you are, I dunno. 
+
+powershell -noprofile -executionpolicy bypass -windowstyle hidden -command "(new-object -com internetexplorer.application).visible=$true"
